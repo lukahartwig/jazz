0c4b30a7175940ac70bee99a8a9257a2304482e6
Trisha Lim <hello@trishalim.com>
2025-01-02 12:48:43 +0000

    Add comment

--- homepage/homepage/app/docs/[framework]/[...slug]/page.tsx
+++ homepage/homepage/app/docs/[framework]/[...slug]/page.tsx
@@ -21,6 +21,7 @@ export default async function Page({
 
     const { default: Content, tableOfContents } = mdxSource;
 
+    // Exclude h1 from table of contents
     const tocItems = (tableOfContents as Toc)?.[0]?.children;
 
     return (
