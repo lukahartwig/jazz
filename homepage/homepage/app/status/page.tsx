a58ee23642ecb50cbdfb6586014ab424914de542
Trisha Lim <hello@trishalim.com>
2025-01-06 14:48:12 +0000

    Status page design

--- homepage/homepage/app/status/page.tsx
+++ homepage/homepage/app/status/page.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { Icon } from "gcmp-design-system/src/app/components/atoms/Icon";
 import { HeroHeader } from "gcmp-design-system/src/app/components/molecules/HeroHeader";
 import { HeartIcon } from "lucide-react";
 
@@ -42,21 +43,42 @@ export default async function Page() {
         slogan="Great system status spage by smart people."
       />
 
-      <div className="grid sm:grid-cols-2 lg:grid-cols-5 gap-8">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-12">
         {responseData.results.A.frames.map((frame) => (
-          <div key={frame.schema.fields[1].labels.probe}>
-            <h2 className="text-2xl">
+          <div key={frame.schema.fields[1].labels.probe} className="">
+            <h2 className="mt-2 flex items-center gap-2 text-xl text-stone-900 dark:text-white">
               {startCase(frame.schema.fields[1].labels.probe)}
             </h2>
-            <div className="mt-2">
-              <HeartIcon
-                className={cn(
-                  "w-10 h-10",
+            <div className="mt-3 flex gap-4 items-center">
+              <Icon
+                name="heart"
+                size="4xl"
+                className={
                   frame.data.values[1][0] === 1
                     ? "text-green-500"
-                    : "text-red-500",
-                )}
+                    : "text-red-500"
+                }
               />
+              <div>
+                <p className="flex items-baseline text-stone-900 dark:text-stone-200">
+                  <div className="bg-blue-400 self-center size-2 rounded-full mr-1.5" />
+                  127
+                  <span className="self-baseline text-xs text-stone-500">
+                    ms
+                  </span>
+                </p>
+                <p className="mt-0.5 text-xs">Avg latency</p>
+              </div>
+              <div>
+                <p className="flex items-baseline text-stone-900 dark:text-stone-200">
+                  <div className="bg-red-500 self-center size-2 rounded-full mr-1.5" />
+                  337
+                  <span className="self-baseline text-xs text-stone-500">
+                    ms
+                  </span>
+                </p>
+                <p className="mt-0.5 text-xs">99th percentile</p>
+              </div>
             </div>
           </div>
         ))}
