a2bb6aa34d31d860f9afa6e31d7e944630858816
Trisha Lim <hello@trishalim.com>
2025-01-06 17:14:57 +0000

    Get latency data over time

--- homepage/homepage/app/status/page.tsx
+++ homepage/homepage/app/status/page.tsx
@@ -1,3 +1,4 @@
+import { CodeGroup } from "@/components/forMdx";
 import { cn } from "@/lib/utils";
 import { clsx } from "clsx";
 import { Icon } from "gcmp-design-system/src/app/components/atoms/Icon";
@@ -8,32 +9,6 @@ import { Fragment } from "react";
 export const metadata = {
   title: "Status",
   description: "Great apps by smart people.",
-};
-
-const locations = [
-  {
-    name: "Edinburgh",
-    people: [
-      {
-        name: "Lindsay Walton",
-        title: "Front-end Developer",
-        email: "lindsay.walton@example.com",
-        role: "Member",
-      },
-      {
-        name: "Courtney Henry",
-        title: "Designer",
-        email: "courtney.henry@example.com",
-        role: "Admin",
-      },
-    ],
-  },
-  // More people...
-];
-
-const statuses = {
-  up: "text-green-400 bg-green-400/10",
-  down: "text-rose-400 bg-rose-400/10",
 };
 
 export default async function Page() {
@@ -59,10 +34,45 @@ export default async function Page() {
           intervalMs: 1000,
           refId: "A",
         },
+        {
+          datasource: {
+            type: "prometheus",
+            uid: "grafanacloud-prom",
+          },
+          editorMode: "code",
+          expr: '1000 / 2 * avg(probe_duration_seconds{probe=~".*", instance="https://mesh.jazz.tools/self-sync-check", job="self-sync-check"} * on (instance, job,probe,config_version) group_left probe_success{probe=~".*",instance="https://mesh.jazz.tools/self-sync-check", job="self-sync-check"} > 0) by (probe)',
+          instant: false,
+          interval: "",
+          intervalFactor: 1,
+          legendFormat: "{{probe}}",
+          refId: "B",
+        },
       ],
     }),
   });
+
   const responseData = await res.json();
+
+  const byProbe: any[] = [];
+
+  for (const frame of responseData.results.A.frames) {
+    const probe = frame.schema.fields[1].labels.probe;
+    byProbe[probe] = {
+      status: frame.data.values[1][0],
+      label: startCase(probe),
+    };
+  }
+
+  for (const frame of responseData.results.B.frames) {
+    const probe = frame.schema.fields[1].labels.probe;
+    if (!byProbe[probe]) {
+      byProbe[probe] = {
+        label: startCase(probe),
+      };
+    }
+
+    byProbe[probe].latencyOverTime = frame.data.values;
+  }
 
   return (
     <div className="container flex flex-col gap-6 pb-10 lg:pb-20">
@@ -70,6 +80,8 @@ export default async function Page() {
         title="Systems status"
         slogan="Great system status spage by smart people."
       />
+
+      {/*<CodeGroup>{JSON.stringify(responseData, null, 2)}</CodeGroup>*/}
 
       <table className="min-w-full">
         <thead className="text-left text-sm font-semibold text-stone-900 dark:text-white">
@@ -92,28 +104,30 @@ export default async function Page() {
           </tr>
         </thead>
         <tbody>
-          {responseData.results.A.frames.map((frame) => (
-            <tr key={frame.schema.fields[1].labels.probe} className="border-t">
-              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-stone-900 sm:pl-3"></td>
+          {Object.values(byProbe).map((row) => (
+            <tr key={row.probe} className="border-t">
+              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-stone-900 sm:pl-3">
+                {row.probe}
+              </td>
               <td className="whitespace-nowrap px-3 py-4 text-sm">100ms</td>
-              <td className="whitespace-nowrap px-3 py-4 text-sm">200ms</td>
+              <td className="whitespace-nowrap px-3 py-4 text-sm">100ms</td>
               <td className="whitespace-nowrap px-3 py-4 text-sm">
                 <div className="flex items-center gap-2">
                   <div
                     className={clsx(
                       "flex-none rounded-full p-1",
-                      frame.data.values[1][0] === 1
+                      row.status === 1
                         ? "text-green-400 bg-green-400/10"
                         : "text-rose-400 bg-rose-400/10",
                     )}
                   >
                     <div className="size-1.5 rounded-full bg-current" />
                   </div>
-                  {frame.data.values[1][0] === 1 ? "Up" : "Down"}
+                  {row.status === 1 ? "Up" : "Down"}
                 </div>
               </td>
               <td className="whitespace-nowrap px-3 py-4 text-sm">
-                {startCase(frame.schema.fields[1].labels.probe)}
+                {row.label}
               </td>
             </tr>
           ))}
