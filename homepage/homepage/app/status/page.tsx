17fec793e6858144febd933aa295bde6ab3bf4ca
Trisha Lim <hello@trishalim.com>
2025-01-06 18:20:12 +0000

    Style heatmap chart

--- homepage/homepage/app/status/page.tsx
+++ homepage/homepage/app/status/page.tsx
@@ -100,7 +100,7 @@ export default async function Page() {
         <tbody>
           {Object.values(byProbe).map((row) => (
             <tr key={row.label} className="border-t">
-              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-stone-900 sm:pl-3">
+              <td className="pr-3">
                 <LatencyChart data={row} />
               </td>
               <td className="whitespace-nowrap px-3 py-4 text-sm">100ms</td>
--- homepage/homepage/components/LatencyChart.tsx
+++ homepage/homepage/components/LatencyChart.tsx
@@ -20,19 +20,125 @@ export function LatencyChart({ data }: { data: any }) {
 
   const chartData = {
     options: {
+      grid: {
+        padding: {
+          left: 0,
+          right: 0,
+        },
+      },
       chart: {
         height: 350,
         type: "heatmap",
+        toolbar: {
+          show: false,
+        },
       },
       dataLabels: {
         enabled: false,
       },
-      colors: ["#008FFB"],
       title: {
-        text: "HeatMap Chart (Single color)",
+        text: "",
       },
+      xaxis: {
+        show: false,
+        labels: {
+          show: false,
+        },
+        axisBorder: {
+          show: false,
+        },
+        axisTicks: {
+          show: false,
+        },
+      },
+      yaxis: {
+        show: false,
+        labels: {
+          show: false,
+        },
+        axisBorder: {
+          show: false,
+        },
+        axisTicks: {
+          show: false,
+        },
+      },
+      plotOptions: {
+        heatmap: {
+          colorScale: {
+            ranges: [
+              {
+                from: 0,
+                to: 10,
+                color: "#1ae200",
+              },
+              {
+                from: 10,
+                to: 50,
+                color: "#32c119",
+              },
+              {
+                from: 50,
+                to: 100,
+                color: "#62bd56",
+              },
+              {
+                from: 100,
+                to: 200,
+                color: "#4c8944",
+              },
+              {
+                from: 200,
+                to: 300,
+                color: "#3b6537",
+              },
+              {
+                from: 300,
+                to: 400,
+                color: "#405b3d",
+              },
+              {
+                from: 400,
+                to: 500,
+                color: "#395335",
+              },
+              {
+                from: 500,
+                to: 750,
+                color: "#283e2b",
+              },
+              {
+                from: 750,
+                to: 1000,
+                color: "#1e2a1d",
+              },
+              {
+                from: 1000,
+                to: 3000,
+                color: "#162018",
+              },
+              {
+                from: 3000,
+                color: "#ff001e",
+              },
+            ],
+          },
+        },
+      },
+      // colors: [
+      //   "#f0fdf4",
+      //   "#dcfce7",
+      //   "#bbf7d0",
+      //   "#86efac",
+      //   "#4ade80",
+      //   "#22c55e",
+      //   "#16a34a",
+      //   "#15803d",
+      //   "#166534",
+      //   "#14532d",
+      //   "#052e16",
+      // ],
     },
-    series: [],
   };
 
   return (
@@ -40,8 +146,8 @@ export function LatencyChart({ data }: { data: any }) {
       options={chartData.options}
       series={[series]}
       type="heatmap"
-      width={500}
-      height={320}
+      width={900}
+      height={100}
     />
   );
 }
