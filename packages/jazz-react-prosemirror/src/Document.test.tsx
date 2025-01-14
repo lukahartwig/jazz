576d2f8f67d15d073c56bf78653eee2f70fedf70
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2025-01-14 14:02:11 +0000

    Fix exports

--- packages/jazz-react-prosemirror/src/Document.test.tsx
+++ packages/jazz-react-prosemirror/src/Document.test.tsx
@@ -1,10 +1,10 @@
 import { render, screen } from "@testing-library/react";
+import { Document } from "jazz-browser-prosemirror";
 import { JazzTestProvider, createJazzTestAccount } from "jazz-react/testing";
 import { ID } from "jazz-tools";
-import React from "react";
+import * as React from "react";
 import { describe, expect, it } from "vitest";
 import { DocumentComponent } from "./index.js";
-import { Document } from "./schema.js";
 
 describe("Document", () => {
   it("renders without crashing", async () => {
--- packages/jazz-react-prosemirror/src/index.ts
+++ packages/jazz-react-prosemirror/src/index.ts
@@ -1,2 +1,2 @@
 export { Document } from "jazz-browser-prosemirror";
-export { DocumentComponent } from "./Document.tsx";
+export { DocumentComponent } from "./Document.js";
