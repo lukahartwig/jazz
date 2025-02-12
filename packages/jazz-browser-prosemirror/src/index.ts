f4fd876247bde5abd8e1125477749c2919969110
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2025-02-12 08:45:34 +0000

    Removes unused transactions fns

--- packages/jazz-browser-prosemirror/src/index.ts
+++ packages/jazz-browser-prosemirror/src/index.ts
@@ -1,4 +1,3 @@
 export { richTextToProsemirrorDoc } from "./lib/document.js";
-export { applyTrToRichText } from "./lib/transactions.js";
 export type * from "./lib/types.js";
 export { createJazzPlugin } from "./lib/plugin.js";
