ea2e2a7f424a0f85477a08661d84ab96024984de
Trisha Lim <hello@trishalim.com>
2025-02-25 20:11:21 +0700

    clean up

--- examples/image-upload/src/ImageUpload.tsx
+++ examples/image-upload/src/ImageUpload.tsx
@@ -1,25 +1,15 @@
 import { createImage } from "jazz-browser-media-images";
 import { ProgressiveImg, useAccount } from "jazz-react";
-import { ImageDefinition } from "jazz-tools";
 import { ChangeEvent, useEffect, useRef, useState } from "react";
-
-function Image({ image }: { image: ImageDefinition }) {
-  return (
-    <ProgressiveImg image={image}>
-      {({ src }) => <img alt="" src={src} className="w-full h-auto" />}
-    </ProgressiveImg>
-  );
-}
 
 export default function ImageUpload() {
   const { me } = useAccount();
-  const [isUploading, setIsUploading] = useState(false);
-  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [imagePreviewUrl, setImagePreviewUrl] = useState<string | null>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     const handleBeforeUnload = (e: BeforeUnloadEvent) => {
-      if (isUploading) {
+      if (imagePreviewUrl) {
         e.preventDefault();
         return "Upload in progress. Are you sure you want to leave?";
       }
@@ -28,12 +18,12 @@ export default function ImageUpload() {
     window.addEventListener("beforeunload", handleBeforeUnload);
     return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
-      // Cleanup preview URL when component unmounts
-      if (previewUrl) {
-        URL.revokeObjectURL(previewUrl);
+
+      if (imagePreviewUrl) {
+        URL.revokeObjectURL(imagePreviewUrl);
       }
     };
-  }, [isUploading, previewUrl]);
+  }, [imagePreviewUrl]);
 
   const onImageChange = async (event: ChangeEvent<HTMLInputElement>) => {
     if (!me?.profile) return;
@@ -41,27 +31,18 @@ export default function ImageUpload() {
     const file = event.currentTarget.files?.[0];
 
     if (file) {
-      // Cleanup any existing preview URL
-      if (previewUrl) {
-        URL.revokeObjectURL(previewUrl);
-      }
-
-      // Create preview URL
       const objectUrl = URL.createObjectURL(file);
-      setPreviewUrl(objectUrl);
+      setImagePreviewUrl(objectUrl);
 
       try {
-        setIsUploading(true);
         me.profile.image = await createImage(file, {
           owner: me.profile._owner,
         });
       } catch (error) {
         console.error("Error uploading image:", error);
       } finally {
-        setIsUploading(false);
-        // Only cleanup preview URL and reset state after successful upload
         URL.revokeObjectURL(objectUrl);
-        setPreviewUrl(null);
+        setImagePreviewUrl(null);
       }
     }
   };
@@ -71,15 +52,12 @@ export default function ImageUpload() {
     me.profile.image = null;
   };
 
-  // states
-  // 1. empty - show image upload form form
-  // 2. uploading - opacity-50, show spinner
-  // 3. uploaded - show delete
-
   if (me?.profile?.image) {
     return (
       <>
-        <Image image={me.profile.image} />
+        <ProgressiveImg image={me.profile.image}>
+          {({ src }) => <img alt="" src={src} className="w-full h-auto" />}
+        </ProgressiveImg>
 
         <button type="button" onClick={deleteImage} className="mt-5">
           Delete image
@@ -88,14 +66,14 @@ export default function ImageUpload() {
     );
   }
 
-  if (previewUrl) {
+  if (imagePreviewUrl) {
     return (
       <div className="relative">
         <p className="z-10 absolute font-semibold text-gray-900 inset-0 flex items-center justify-center">
           Uploading image...
         </p>
         <img
-          src={previewUrl}
+          src={imagePreviewUrl}
           alt="Preview"
           className="opacity-50 w-full h-auto"
         />
--- examples/image-upload/tailwind.config.ts
+++ examples/image-upload/tailwind.config.ts
@@ -1,6 +1,4 @@
 import type { Config } from "tailwindcss";
-import plugin from "tailwindcss/plugin";
-const colors = require("tailwindcss/colors");
 
 const config: Config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -15,27 +13,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [
-    plugin(({ addBase }) =>
-      addBase({
-        ":root": {
-          "--gcmp-border-color": colors.stone[200],
-          "--gcmp-invert-border-color": colors.stone[900],
-        },
-        "*": {
-          borderColor: "var(--gcmp-border-color)",
-        },
-        "@media (prefers-color-scheme: dark)": {
-          "*": {
-            borderColor: "var(--gcmp-invert-border-color)",
-          },
-        },
-        "*:focus": {
-          outline: "none",
-        },
-      }),
-    ),
-  ],
 } as const;
 
 export default config;
