2d5badadc668e41778904b10a607980ee7592cbb
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2025-04-16 12:13:46 +0100

    Add filter against main

--- ignore-vercel-build.js
+++ ignore-vercel-build.js
@@ -4,18 +4,20 @@ const currentAppName = process.env.APP_NAME || process.argv[2];
 
 try {
   // In Vercel CI, we need to use the remote cache and filter for the current app
-  const turboCommand = `pnpm turbo run build --dry=json --filter=${currentAppName}`;
+  const turboCommand = `pnpm turbo run build --dry=json --filter=${currentAppName}...[origin/main]`;
   const turboOutput = execSync(turboCommand).toString();
 
   const affectedPackages = JSON.parse(turboOutput).packages;
   const shouldBuild = affectedPackages.includes(currentAppName);
 
   if (shouldBuild) {
-    console.log(`✅ Building ${currentAppName} as it is affected by changes.`);
+    console.log(
+      `✅ Building "${currentAppName}" as it is affected by changes.`,
+    );
     process.exit(1); // Continue with the build
   } else {
     console.log(
-      `🛑 Skipping build for ${currentAppName} as it is not affected by changes.`,
+      `🛑 Skipping build for "${currentAppName}" as it is not affected by changes.`,
     );
     process.exit(0); // Skip the build
   }
@@ -23,6 +25,6 @@ try {
   // If we can't parse the turbo output or something goes wrong,
   // we should build to be safe
   console.log(`⚠️ Error determining affected packages: ${error.message}`);
-  console.log(`✅ Proceeding with build for ${currentAppName} to be safe.`);
+  console.log(`⚠️ Proceeding with build for "${currentAppName}" to be safe.`);
   process.exit(1);
 }
