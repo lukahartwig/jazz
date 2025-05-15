6794e0d90ed47eb566e1065091c405f634252534
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2025-05-15 15:49:01 +0100

    Adds Groups doc

--- homepage/homepage/content/docs/docNavigationItems.js
+++ homepage/homepage/content/docs/docNavigationItems.js
@@ -57,7 +57,7 @@ export const docNavigationItems = [
       },
       {
         name: "Groups and ownership",
-        href: "/docs/building-with-jazz/groups-and-ownership",
+        href: "/docs/building-with-jazz/groups",
         done: 100,
       },
       {
