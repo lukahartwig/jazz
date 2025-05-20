69ba1f9348c1350695aec4926fa0282121aa2e05
Brad Anderson <brad@sankatygroup.com>
2025-05-20 00:03:15 -0400

    chore: metro & babel changes

--- examples/chat-rn-expo/metro.config.js
+++ examples/chat-rn-expo/metro.config.js
@@ -19,10 +19,33 @@ config.resolver.nodeModulesPaths = [
   path.resolve(workspaceRoot, "node_modules"),
 ];
 config.resolver.sourceExts = ["mjs", "js", "json", "ts", "tsx"];
+// even though this is true by default, it seems to help zod
+// TODO: revisit if we find a smoking gun, and remove if we can
+config.resolver.unstable_enablePackageExports = true;
+
 config.resolver.requireCycleIgnorePatterns = [
   /(^|\/|\\)node_modules($|\/|\\)/,
   /(^|\/|\\)packages($|\/|\\)/,
 ];
+
+// --- START: Added/Modified section to transpile Zod ---
+// Ensure Zod is transformed by Babel along with other necessary React Native packages.
+// The pattern below is a common one, with 'zod' added to the list of packages
+// that SHOULD be transformed (i.e., not ignored by the transformer).
+config.transformer = {
+  ...config.transformer, // Preserve existing transformer settings if any from getDefaultConfig
+  getTransformOptions: async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: true, // Recommended for React Native for performance
+    },
+  }),
+  // This pattern tells Metro to transform Zod and other common RN/Expo packages.
+  transformIgnorePatterns: [
+    "node_modules/(?!(react-native|@react-native|@react-native-community|expo|@expo|zod)/)",
+  ],
+};
+// --- END: Added/Modified section to transpile Zod ---
 
 // Use turborepo to restore the cache when possible
 config.cacheStores = [
@@ -31,5 +54,4 @@ config.cacheStores = [
   }),
 ];
 
-// module.exports = config;
 module.exports = withNativeWind(config, { input: "./global.css" });
